da6ff07b3c36e64dc6152c98e77f0287e099aa94
imperialMax <max.neal1@education.nsw.gov.au>
2024-11-26 21:09:34 +1100

    nicholas ii of yugoslavia (2nd) commit
    worked on layout and whatnot idk
    lotsa good stuff in here (how tf am i gonna sanitise AND validate all of my inputs theres like 10 gazbattrillion

--- PawfectPetsInterface/PawfectPetsInterface/PawfectPetsInterface.py
+++ PawfectPetsInterface/PawfectPetsInterface/PawfectPetsInterface.py
@@ -4,98 +4,120 @@ ph = PasswordHasher()
 connection_string = (
     r"Driver={ODBC Driver 17 for SQL Server};"
     r"Server=LAPTOP-E8GVKM9S\SQLEXPRESS;"
-    r"Database=VideogamesDB;"
+    r"Database=ElysiumLuxuryDB;"
     r"Trusted_Connection=yes;"
     r"Column Encryption Setting=Enabled;"
     
 )
 
+global verifieduser 
+
+verifieduser = False
+def editSuppliers():
+    supplierTransaction = input("Suppliers:" + "\n" +
+                          "1. Select" + "\n" +
+                          "2. Insert" + "\n" +
+                          "3. Update" + "\n" +
+                          "4. Delete" + "\n")
+    match supplierTransaction:
+        case "1":
+            pass
+        case "2":
+            InsertSuppliers()
+        case "3":
+            pass
+        case "4":
+            pass
+
+def editProducts():
+    productTransaction = input("Products:" + "\n" +
+                          "1. Select" + "\n" +
+                          "2. Insert" + "\n" +
+                          "3. Update" + "\n" +
+                          "4. Delete" + "\n")
+    match productTransaction:
+        case "1":
+            pass
+        case "2":
+            InsertProducts()
+        case "3":
+            pass
+        case "4":
+            pass
+
+def editCustomers():
+    customerTransaction = input("Customers:" + "\n" +
+                          "1. Select" + "\n" +
+                          "2. Insert" + "\n" +
+                          "3. Update" + "\n" +
+                          "4. Delete" + "\n")
+    match customerTransaction:
+        case "1":
+            pass
+        case "2":
+            InsertCustomers()
+        case "3":
+            pass
+        case "4":
+            pass
+
+# for creating new suppliers
 def InsertSuppliers():
-    supplierInput = input("what is the name of the developer ")
+    supplierInput = input("What is the name of the supplier?" + "\n")
+    contactInput = input("What is the contact email of the supplier?" + "\n") ### format the input somehow???? 
+
 
     conn = pyodbc.connect(connection_string)
     
     cursor = conn.cursor()
     
-    insertQuery = """ INSERT INTO developer (designer_name) VALUES (?) """ 
+    insertQuery = """ INSERT INTO suppliers (supplier_name, supplier_email) VALUES (?,?) """ 
     
-    data = (designerInput)
+    data = (supplierInput, contactInput)
 
     cursor.execute(insertQuery,data)
     conn.commit()
     cursor.close()
     conn.close()
 
-def InsertGame():
-    gameNameInput = input("what is the name of the game ")
-    genreInput = input("what is the genre of the game ")
-    releaseDateInput = input("what was the release date of the game YYYY-MM-dd ")
-    costInput = input("what was the cost of the game ")
-    platformInput = input("what platform did the game release on ")
+# for creating new products
+def InsertProducts():
+    productInput = input("What is the name of the product?" + "\n")
+    costInput = input("what is the cost of the product?" + "\n")
+    supplierInput = input("what is the ID of the supplier?" + "\n")
+
+    #######################################################
+    ###################################################### ASK BARTLETT ABOUT FOREIGN KEYS!!??? (Blunder, big mistake, resign now)
 
     conn = pyodbc.connect(connection_string)
     
     cursor = conn.cursor()
     
-    insertQuery = """ INSERT INTO game (game_name, genre, date_of_release, cost, game_platform) VALUES (?, ?, ?, ?, ?) """ 
+    insertQuery = """ INSERT INTO products (supplier_ID, product_name, product_cost) VALUES (?, ?, ?) """ 
     
-    data = (gameNameInput, genreInput, releaseDateInput, costInput, platformInput)
+    data = (supplierInput, productInput, costInput)
 
     cursor.execute(insertQuery,data)
     conn.commit()
     cursor.close()
     conn.close()
 
-def InsertConsoles():
-    designerInput = input("who designed console ")
-    releaseDateInput = input("when was it released? YYYY-MM-dd ")
-    priceInput = input("how much does it cost ")
 
-    conn = pyodbc.connect(connection_string)
-    
-    cursor = conn.cursor()
-    
-    insertQuery = """ INSERT INTO console (designer, date_of_release, price) VALUES (?, ?, ?) """ 
-    
-    data = (designerInput,releaseDateInput,priceInput)
-
-    cursor.execute(insertQuery,data)
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-def InsertDevelopers():
-    developerInput = input("what is the name of the developer ")
-    countryInput = input("what country is the developer from ")
-
-    conn = pyodbc.connect(connection_string)
-    
-    cursor = conn.cursor()
-    
-    insertQuery = """ INSERT INTO developer (dev_name, dev_country) VALUES (?, ?) """ 
-    
-    data = (developerInput, countryInput)
-
-    cursor.execute(insertQuery,data)
-    conn.commit()
-    cursor.close()
-    conn.close()
-
+## yeah well im working on it
 def InsertCustomers():
-    usernameInput = input("Enter Username ")
-    emailInput = input("Enter Email ")
-    passwordOrigin = input("Enter Password ")
-    fullNameInput = input("Enter Full Name (First and last name) ")
-    phoneNumberInput = input("Enter phone number ")
+    firstNameInput = input("What is the customer's first name?" + "\n")
+    lastNameInput = input("What is the customer's last name?" + "\n")
+    emailInput = input("What is the customer's email?" + "\n")
+    addressInput = input("What is the customer's address?" + "\n")
+    phoneNumberInput = input("What is the customer's phone number?" )
 
-    passwordHash = ph.hash(passwordOrigin)
-    print(passwordHash)
+
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor()
 
-    insertQuery = """ INSERT INTO users (username, email, password, full_name, phone_number) VALUES (?, ?, ?, ?, ?) """
+    insertQuery = """ INSERT INTO customers (first_name, last_name, email, address, phone_number) VALUES (?, ?, ?, ?, ?) """
 
-    data = (usernameInput, emailInput, passwordHash, fullNameInput, phoneNumberInput)
+    data = (firstNameInput, lastNameInput, emailInput, addressInput, phoneNumberInput)
 
     cursor.execute(insertQuery,data)
     conn.commit()
@@ -103,16 +125,34 @@ def InsertCustomers():
     conn.close()
 ##deletions
 
+def createUser():
+    username = input("What will your username be?" + "\n")
+    password = input("What will your password be?" + "\n")
+
+    passwordHash = ph.hash(password)
+
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    insertQuery = """ INSERT INTO administrators (username, password) VALUES (?,?) """
+
+    data = (username, passwordHash)
+
+    cursor.execute(insertQuery, data)
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 def verifyUser():
+    global verifieduser
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor()
     try:
-        username = input("Enter username: ")
-        password = input("Enter password: ")
+        username = input("What is your username?" + "\n")
+        password = input("What is your password?" + "\n")
 
         select_query = '''
-            SELECT password_hash FROM users WHERE username = ?
+            SELECT password FROM administrators WHERE username = ?
         '''
 
         cursor.execute(select_query, (username,))
@@ -121,7 +161,9 @@ def verifyUser():
         if result:
             password_hash = result[0]
             if ph.verify(password_hash, password):
-                print("password is correct")
+                
+                verifieduser = True
+                
             else:
                 print("password is incorrect")
         else:
@@ -132,24 +174,40 @@ def verifyUser():
         cursor.close()
 
 
-uInput = input("Welcome to the Videogames Database" + "\n" +
-               "Please Select an option" + "\n" + 
-               "1. Insert user" + "\n" +
-               "2. Insert developer" + "\n" +
-               "3. Insert game" + "\n" +
-               "4. Insert console" + "\n" +
-               "5. Insert designer" + "\n" +
-               "6. Verify user" + "\n")
-match uInput:
-    case "1":
-        InsertUsers()
-    case "2":
-        InsertDevelopers()
-    case "3":
-        InsertGame()
-    case "4":
-        InsertConsoles()
-    case "5":
-        InsertDesigners()
-    case "6":
-        verifyUser()
+while True:
+    if verifieduser == True:
+        break
+    else:
+
+        loginOrCreateAccount = input("Do you wish to login or create an account?" + "\n" + 
+                                 "Please select an option by entering the corresponding number:" + "\n" +
+                                 "1. Login" + "\n" +
+                                 "2. Create Account" + "\n")
+
+        match loginOrCreateAccount:
+            case "1":
+                
+                verifyUser()
+            case "2":
+                createUser()
+                createdaccount = True
+
+
+
+while True:
+
+    uInput = input("Welcome to the Pawfect Pets database" + "\n" +
+                       "Please Select an option" + "\n" + 
+                       "1. Edit Products" + "\n" +
+                       "2. Edit Suppliers" + "\n" +
+                       "3. Edit Customers" + "\n")
+    match uInput:
+        case "1":
+            editProducts()
+        case "2":
+            editSuppliers()
+        case "3":
+            editCustomers()
+
+
+
