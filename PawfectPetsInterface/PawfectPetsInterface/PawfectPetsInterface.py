79baf79f5c7c81e02ece6a0b693ceddc71c7a388
imperialMax <max.neal1@education.nsw.gov.au>
2024-11-27 21:23:46 +1100

    tsar nicholas iii (third)
    made the select transactions work and started on the update transactions... have to ask sir about the foreign keys and other transactions.

--- PawfectPetsInterface/PawfectPetsInterface/PawfectPetsInterface.py
+++ PawfectPetsInterface/PawfectPetsInterface/PawfectPetsInterface.py
@@ -12,6 +12,8 @@ connection_string = (
 
 global verifieduser 
 
+
+
 verifieduser = False
 def editSuppliers():
     supplierTransaction = input("Suppliers:" + "\n" +
@@ -21,11 +23,11 @@ def editSuppliers():
                           "4. Delete" + "\n")
     match supplierTransaction:
         case "1":
-            pass
+            SelectSuppliers()
         case "2":
             InsertSuppliers()
         case "3":
-            pass
+            UpdateSuppliers()
         case "4":
             pass
 
@@ -37,7 +39,7 @@ def editProducts():
                           "4. Delete" + "\n")
     match productTransaction:
         case "1":
-            pass
+            SelectProducts()
         case "2":
             InsertProducts()
         case "3":
@@ -53,7 +55,7 @@ def editCustomers():
                           "4. Delete" + "\n")
     match customerTransaction:
         case "1":
-            pass
+            SelectCustomers()
         case "2":
             InsertCustomers()
         case "3":
@@ -61,7 +63,8 @@ def editCustomers():
         case "4":
             pass
 
-# for creating new suppliers
+
+### SUPPLIERS
 def InsertSuppliers():
     supplierInput = input("What is the name of the supplier?" + "\n")
     contactInput = input("What is the contact email of the supplier?" + "\n") ### format the input somehow???? 
@@ -80,14 +83,92 @@ def InsertSuppliers():
     cursor.close()
     conn.close()
 
-# for creating new products
+def SelectSuppliers():
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+    uInput = input("Would you like to view the entire table or just one row?" + "\n" + "Select an option by entering the corresponding number:"
+                   + "\n" + "1. Entire table" + "\n" + "2. One row" + "\n")
+    match uInput:
+        case "1":
+            select_Query = ''' 
+                SELECT * FROM suppliers
+            '''
+            cursor.execute(select_Query)
+            result = cursor.fetchall()
+            resultList = [result]
+            print("Here are the results of your search:" + "\n")
+            print(resultList)
+            print("\n")
+        case "2":
+            uInput = input("Which row would you like to view?" + "\n" + "Select an option by entering the corresponding number:"
+                   + "\n" + "1. supplier_name" + "\n" + "2. supplier_email" + "\n")
+            match uInput:
+                case "1":
+                    select_Query = ''' 
+                        SELECT supplier_name FROM suppliers
+                    '''
+                    cursor.execute(select_Query)
+                    result = cursor.fetchall()
+                    resultList = [result]
+                    print("Here are the results of your search:" + "\n")
+                    print(resultList)
+                    print("\n")
+                case "2":
+                    select_Query = ''' 
+                        SELECT supplier_email FROM suppliers
+                    '''
+                    cursor.execute(select_Query)
+                    result = cursor.fetchall()
+                    resultList = [result]
+                    print("Here are the results of your search:" + "\n")
+                    print(resultList)
+                    print("\n")
+    cursor.close()
+    conn.close()
+
+def UpdateSuppliers():
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    select_Query = ''' 
+        SELECT * FROM suppliers     
+    '''
+    cursor.execute(select_Query)
+    result = cursor.fetchall()
+    resultList = [result]
+
+    print("Here is the contents of the table:" + "\n")
+    print(resultList)
+    print(resultList)
+    uInput = input("\n" + "What would you like to update?" + "\n" + "Select an option by entering the corresponding number:" 
+          + "\n" + "1. supplier_name"
+          + "\n" + "2. supplier_email" + "\n")
+    supplier_ID = input("What is the ID of the supplier that you want to edit?"
+          + "\n" + "Select an option by entering the number appearing at the front of the supplier's list:"
+          + "\n")
+    match uInput:
+        case "1":
+            updatedValue = input("The supplier_name is currently " + resultList[1] + "." 
+                                 + "\n" + "What should the updated value be?" 
+                                 + "\n")
+            update_Query = """
+                UPDATE suppliers SET supplier_name WHERE supplier_ID VALUES (?,?) 
+            """
+
+            data = (updatedValue,supplier_ID)
+
+            cursor.execute(update_Query,data)
+            conn.commit()
+            cursor.close()
+            conn.close()
+### PRODUCTS
 def InsertProducts():
     productInput = input("What is the name of the product?" + "\n")
     costInput = input("what is the cost of the product?" + "\n")
     supplierInput = input("what is the ID of the supplier?" + "\n")
 
     #######################################################
-    ###################################################### ASK BARTLETT ABOUT FOREIGN KEYS!!??? (Blunder, big mistake, resign now)
+    #######################################################x`lookup supplier, produce list?? list must be names not keys
 
     conn = pyodbc.connect(connection_string)
     
@@ -95,21 +176,63 @@ def InsertProducts():
     
     insertQuery = """ INSERT INTO products (supplier_ID, product_name, product_cost) VALUES (?, ?, ?) """ 
     
-    data = (supplierInput, productInput, costInput)
+    data = (supplierInput, 
+            productInput, costInput)
 
     cursor.execute(insertQuery,data)
     conn.commit()
     cursor.close()
     conn.close()
 
-
-## yeah well im working on it
+def SelectProducts():
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+    uInput = input("Would you like to view the entire table or just one row?" + "\n" + "Select an option by entering the corresponding number:"
+                   + "\n" + "1. Entire table" + "\n" + "2. One row" + "\n")
+    match uInput:
+        case "1":
+            select_Query = ''' 
+                SELECT * FROM products
+            '''
+            cursor.execute(select_Query)
+            result = cursor.fetchall()
+            resultList = [result]
+            print("Here are the results of your search:" + "\n")
+            print(resultList)
+            print("\n")
+        case "2":
+            uInput = input("Which row would you like to view?" + "\n" + "Select an option by entering the corresponding number:"
+                   + "\n" + "1. product_name" + "\n" + "2. product_cost" + "\n")
+            match uInput:
+                case "1":
+                    select_Query = ''' 
+                        SELECT product_name FROM products
+                    '''
+                    cursor.execute(select_Query)
+                    result = cursor.fetchall()
+                    resultList = [result]
+                    print("Here are the results of your search:" + "\n")
+                    print(resultList)
+                    print("\n")
+                case "2":
+                    select_Query = ''' 
+                        SELECT product_cost FROM products
+                    '''
+                    cursor.execute(select_Query)
+                    result = cursor.fetchall()
+                    resultList = [result]
+                    print("Here are the results of your search:" + "\n")
+                    print(resultList)
+                    print("\n")
+    cursor.close()
+    conn.close()
+### CUSTOMERS
 def InsertCustomers():
     firstNameInput = input("What is the customer's first name?" + "\n")
     lastNameInput = input("What is the customer's last name?" + "\n")
     emailInput = input("What is the customer's email?" + "\n")
     addressInput = input("What is the customer's address?" + "\n")
-    phoneNumberInput = input("What is the customer's phone number?" )
+    phoneNumberInput = input("What is the customer's phone number?" + "\n")
 
 
     conn = pyodbc.connect(connection_string)
@@ -123,7 +246,87 @@ def InsertCustomers():
     conn.commit()
     cursor.close()
     conn.close()
-##deletions
+
+def SelectCustomers():
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+    uInput = input("Would you like to view the entire table or just one row?" + "\n" + "Select an option by entering the corresponding number:"
+                   + "\n" + "1. Entire table" + "\n" + "2. One row" + "\n")
+    match uInput:
+        case "1":
+            select_Query = ''' 
+                SELECT * FROM customers
+            '''
+            cursor.execute(select_Query)
+            result = cursor.fetchall()
+            resultList = [result]
+            print("Here are the results of your search:" + "\n")
+            print(resultList)
+            print("\n")
+        case "2":
+            uInput = input("Which row would you like to view?" + "\n" + "Select an option by entering the corresponding number:"
+                   + "\n" + "1. first_name" 
+                   + "\n" + "2. last_name" 
+                   + "\n" + "3. email"
+                   + "\n" + "4. address"
+                   + "\n" + "5. phone_number"
+                   + "\n")
+            match uInput:
+                case "1":
+                    select_Query = ''' 
+                        SELECT first_name FROM customers
+                    '''
+                    cursor.execute(select_Query)
+                    result = cursor.fetchall()
+                    resultList = [result]
+                    print("Here are the results of your search:" + "\n")
+                    print(resultList)
+                    print("\n")
+                case "2":
+                    select_Query = ''' 
+                        SELECT last_name FROM customers
+                    '''
+                    cursor.execute(select_Query)
+                    result = cursor.fetchall()
+                    resultList = [result]
+                    print("Here are the results of your search:" + "\n")
+                    print(resultList)
+                    print("\n")
+                case "3":
+                    select_Query = ''' 
+                        SELECT email FROM customers
+                    '''
+                    cursor.execute(select_Query)
+                    result = cursor.fetchall()
+                    resultList = [result]
+                    print("Here are the results of your search:" + "\n")
+                    print(resultList)
+                    print("\n")
+                case "4":
+                    select_Query = ''' 
+                        SELECT address FROM customers
+                    '''
+                    cursor.execute(select_Query)
+                    result = cursor.fetchall()
+                    resultList = [result]
+                    print("Here are the results of your search:" + "\n")
+                    print(resultList)
+                    print("\n")
+                case "5":
+                    select_Query = ''' 
+                        SELECT phone_number FROM customers
+                    '''
+                    cursor.execute(select_Query)
+                    result = cursor.fetchall()
+                    resultList = [result]
+                    print("Here are the results of your search:" + "\n")
+                    print(resultList)
+                    print("\n")
+
+
+
+    cursor.close()
+    conn.close()
 
 def createUser():
     username = input("What will your username be?" + "\n")
@@ -142,7 +345,7 @@ def createUser():
     conn.commit()
     cursor.close()
     conn.close()
-
+    
 def verifyUser():
     global verifieduser
     conn = pyodbc.connect(connection_string)
@@ -197,7 +400,7 @@ while True:
 while True:
 
     uInput = input("Welcome to the Pawfect Pets database" + "\n" +
-                       "Please Select an option" + "\n" + 
+                       "Please select an option by entering the corresponding number" + "\n" + 
                        "1. Edit Products" + "\n" +
                        "2. Edit Suppliers" + "\n" +
                        "3. Edit Customers" + "\n")
